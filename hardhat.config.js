3efda63fab8914435766a5ff0b700cad35c821b7
mikema <mikema@1token.trade>
2023-09-05 16:56:01 +0800

    fork主网配置

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks:{
+    hardhat:{
+      forking:{
+        url:"https://eth-mainnet.g.alchemy.com/v2/WFi5mZksA_qYP_XDqaLQ8U6refMMc9Z8",
+      }
+    }
+  }
 };
